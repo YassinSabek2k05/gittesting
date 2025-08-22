f946ef838e55a496f64c16ca06e5df57a88341c9
yassinsabek <yassinsabek@gmail.com>
2025-08-22 06:02:10 +0300

    First commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args){
+        int k = 5;
+        int l=0;
+        for(int i=0;i<=5;i+=1){
+            String str = "";
+            for(int j=0;j<(k-i);j++){
+                str+=" ";
+            }
+            for(int j=0;j<2*l;j++){
+                str+="*";
+            }
+            l+=1;
+            str+="*";
+            System.out.println("-------------------");
+            
+            System.out.println(str);
+        }
+    }
+}
